e41639fc29cfaf6030d4364956b4ba1d40ac5bd5
MaryamAlEisah <maryam.aleisah@gmail.com>
2021-05-01 06:54:15 +0300

    lab-06

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let seattle = {
+    min_max_hour1dCoustomers:0;
+    Average_Cookies:0;
+    getCust: function(){
+        this.cust= randomave(23, 65) + 'customer';
+        conssole.log(this.cust);
+    }
+}
+
+
