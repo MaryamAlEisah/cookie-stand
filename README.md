f9ad2f7ffa9c90a35ca5a715a6eeaa0a6ef10a7f
MaryamAlEisah <maryam.aleisah@gmail.com>
2021-05-04 16:38:36 +0300

    class07-Objects

--- README.md
+++ README.md
@@ -17,3 +17,18 @@ The average number of cookies purchased per customer.
 Because we are early in the life of this business, we will need to build an application that is adaptable. Pat will need to be able to add and remove locations from the daily projections report, and Pat will also need to be able to easily modify the input numbers for each location based on the day of the week, special events, and other factors. Pat would like to see these numbers with nice formatting in a web application.
 Pat needs you to take a leading role in doing the design work and construction of a public-facing page, too. They already have a logo image picked out (below), but Pat is requesting your assistance in the design of the documents, color scheme, fonts, and any additional images for the public-facing site.
 
+
+
+# Lab-07: Lab: Salmon Cookies Pt. 2
+
+Problem Domain
+Pat’s Salmon Cookies problem domain can be found here!
+
+Instructions
+Create a new branch for today’s lab. Make sure it has all of your changes from lab 06 so that you can extend the functionality.
+
+Replace all of your object literals for the salmon cookie stand with a single constructor function that, when called with the ‘new’ keyword, it creates a new instance.
+
+Replace the lists of your data for each store and build a single table of data instead. It should look similar to the following:
+
+Display each stores data in a table format similar to what is below. Break each column by the hour and complete each row with a “Daily Location Total”.
--- js/app.js
+++ js/app.js
@@ -1,330 +1,55 @@
 'use strict';
-
+console.log('hello');
 let hours= ['6am', '7am', '8am', '9am', '10am' , '11am' , '12pm' , '1pm' , '2pm' , '3pm' , '4pm' , '5pm' , '6pm' , '7pm'];
 let list=document.getElementById("lists1");
 function avgCust(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
  }
-let Seattle = {
-    locationName : 'seattle',
-    min_max_hourlyCoustomers:0,
-    Average_Cookies:6.3,
-    total:0,
-    sales:[],
-    customerPerHour:[],
-    getCust: function() {
-       return this.min_max_hourlyCoustomers= avgCust(23, 65) ;
-    },
-     getCookies: function(){
-       for(let i=0 ; i < hours.length ; i++)
-        {
-           this.sales.push(this.customerPerHour[i]*this.Average_Cookies);
-         }
-     },
-    getCoustomerPerHour: function()
-    {
-        for(let i = 0 ; i < hours.length ; i++){
-        this.customerPerHour.push(this.getCust());
-       console.log(this.customerPerHour[i]+" custome per hour")
-        }
-    },
-    getCokiesAvg : function()
-    {
-        for(let k = 0 ; k < hours.length ; k++){
-            this.getCoustomerPerHour();
-            let resulets=Math.floor(this.customerPerHour[k] * this.Average_Cookies )
-            this.sales.push(resulets);
-            this.total+=resulets;
-       console.log(this.Average_Cookies);
-        console.log(this.sales[k]+" cookies sold")
-        }
-    },
-    render : function()
-    {
-        let pe1=document.createElement('p');
-        list.appendChild(pe1);
-        let ule1=document.createElement('ul')
-        list.appendChild(ule1);
-        pe1.textContent=this.locationName;
-        for(let j=0 ; j <hours.length ; j++)
-        {
-            let lie1=document.createElement('li');
-            ule1.appendChild(lie1);
-            if(TimeRanges.length -1 === j)
-             {
-                lie1.textContent=`${hours[j]} : ${this.total} cookies`;
-            }
-                lie1.textContent=`${hours[j]} : ${this.sales[j]} cookies`;
-                this.total=this.total+this.sales[j];
-        }
-        let liRsults=document.createElement('li');
-        ule1.appendChild(liRsults)
-        liRsults.textContent=`total : ${this.total}`;
-    }
-};
+// global array
+let locations = [];
 
-Seattle.getCoustomerPerHour();
-Seattle.getCokiesAvg();
- Seattle.render();
+ //Constructor function
 
-// -------------------------------------------------------------------------------
+ function SalmonCookies (locationName, minCustPerHour, maxCustPerHour, avgCookiesPerCust){
+    this.locationName= locationName; 
+    this.minCustPerHour=minCustPerHour;
+    this.maxCustPerHour=maxCustPerHour;
+    this.avgCookiesPerCust= avgCookiesPerCust;
+     
 
-let Tokyo = {
-    locationName : 'Tokyo',
-    min_max_hourlyCoustomers:0,
-    Average_Cookies:1.2,
-    total:0,
-    sales:[],
-    customerPerHour:[],
-    getCust: function() {
-       return this.min_max_hourlyCoustomers= avgCust(3, 24) ;
-    },
-     getCookies: function(){
-       for(let i=0 ; i < hours.length ; i++)
-        {
-           this.sales.push(this.customerPerHour[i]*this.Average_Cookies);
-         }
-     },
-    getCoustomerPerHour: function()
-    {
-        for(let i = 0 ; i < hours.length ; i++){
-        this.customerPerHour.push(this.getCust());
-       console.log(this.customerPerHour[i]+" custome per hour")
-        }
-    },
-    getCokiesAvg : function()
-    {
-        for(let k = 0 ; k < hours.length ; k++){
-            this.getCoustomerPerHour();
-            let resulets=Math.floor(this.customerPerHour[k] * this.Average_Cookies )
-            this.sales.push(resulets);
-            this.total+=resulets;
-       console.log(this.Average_Cookies);
-        console.log(this.sales[k]+" cookies sold")
-        }
-    },
-    render : function()
-    {
-        let pe1=document.createElement('p');
-        list.appendChild(pe1);
-        let ule1=document.createElement('ul')
-        list.appendChild(ule1);
-        pe1.textContent=this.locationName;
-        for(let j=0 ; j <hours.length ; j++)
-        {
-            let lie1=document.createElement('li');
-            ule1.appendChild(lie1);
-            if(TimeRanges.length -1 === j)
-             {
-                lie1.textContent=`${hours[j]} : ${this.total} cookies`;
-            }
-                lie1.textContent=`${hours[j]} : ${this.sales[j]} cookies`;
-                this.total=this.total+this.sales[j];
-                console.log(this.sales[j]);
-        }
-        let liRsults=document.createElement('li');
-        ule1.appendChild(liRsults)
-        liRsults.textContent=`total : ${this.total}`;
-    }
-};
+    // pushing the created objects
 
-Tokyo.getCoustomerPerHour();
-Tokyo.getCokiesAvg();
-Tokyo.render();
+    locations.push(this); 
 
-// //-----------------------------------------------------------------------------------
+ }
+ 
+//Salmon cookies prototype fun
 
-let Dubai = {
-    locationName : 'Dubai',
-    min_max_hourlyCoustomers:0,
-    Average_Cookies:3.7,
-    total:0,
-    sales:[],
-    customerPerHour:[],
-    getCust: function() {
-       return this.min_max_hourlyCoustomers= avgCust(11, 38) ;
-    },
-     getCookies: function(){
-       for(let i=0 ; i < hours.length ; i++)
-        {
-           this.sales.push(this.customerPerHour[i]*this.Average_Cookies);
-         }
-     },
-    getCoustomerPerHour: function()
-    {
-        for(let i = 0 ; i < hours.length ; i++){
-        this.customerPerHour.push(this.getCust());
-       console.log(this.customerPerHour[i]+" custome per hour")
-        }
-    },
-    getCokiesAvg : function()
-    {
-        for(let k = 0 ; k < hours.length ; k++){
-            this.getCoustomerPerHour();
-            let resulets=Math.floor(this.customerPerHour[k] * this.Average_Cookies )
-            this.sales.push(resulets);
-            this.total+=resulets;
-       console.log(this.Average_Cookies);
-        console.log(this.sales[k]+" cookies sold")
-        }
-    },
-    render : function()
-    {
-        let pe1=document.createElement('p');
-        list.appendChild(pe1);
-        let ule1=document.createElement('ul')
-        list.appendChild(ule1);
-        pe1.textContent=this.locationName;
-        for(let j=0 ; j <hours.length ; j++)
-        {
-            let lie1=document.createElement('li');
-            ule1.appendChild(lie1);
-            if(TimeRanges.length -1 === j)
-             {
-                lie1.textContent=`${hours[j]} : ${this.total} cookies`;
-            }
-                lie1.textContent=`${hours[j]} : ${this.sales[j]} cookies`;
-                this.total=this.total+this.sales[j];
-                console.log(this.sales[j]);
-        }
-        let liRsults=document.createElement('li');
-        ule1.appendChild(liRsults)
-        liRsults.textContent=`total : ${this.total}`;
-    }
-};
+SalmonCookies.prototype.Locations = function() {
+    
+    console.log(this.locationName + this.minCustPerHour + this.minCustPerHour + this.maxCustPerHour + this.avgCookiesPerCust );
+}
+let seattle = new SalmonCookies('Seattle',23,65,6.3);
+seattle.Locations();
 
-Dubai.getCoustomerPerHour();
-Dubai.getCokiesAvg(); 
-Dubai.render();
-//------------------------------------------------------------------------------
+let tokyo = new SalmonCookies('Tokyo',3,24,1.2);
 
-let Paris = {
-    locationName : 'Paris',
-    min_max_hourlyCoustomers:0,
-    Average_Cookies:2.3,
-    total:0,
-    sales:[],
-    customerPerHour:[],
-    getCust: function() {
-       return this.min_max_hourlyCoustomers= avgCust(20, 38) ;
-    },
-     getCookies: function(){
-       for(let i=0 ; i < hours.length ; i++)
-        {
-           this.sales.push(this.customerPerHour[i]*this.Average_Cookies);
-         }
-     },
-    getCoustomerPerHour: function()
-    {
-        for(let i = 0 ; i < hours.length ; i++){
-        this.customerPerHour.push(this.getCust());
-       console.log(this.customerPerHour[i]+" custome per hour")
-        }
-    },
-    getCokiesAvg : function()
-    {
-        for(let k = 0 ; k < hours.length ; k++){
-            this.getCoustomerPerHour();
-            let resulets=Math.floor(this.customerPerHour[k] * this.Average_Cookies )
-            this.sales.push(resulets);
-            this.total+=resulets;
-       console.log(this.Average_Cookies);
-        console.log(this.sales[k]+" cookies sold")
-        }
-    },
-    render : function()
-    {
-        let pe1=document.createElement('p');
-        list.appendChild(pe1);
-        let ule1=document.createElement('ul')
-        list.appendChild(ule1);
-        pe1.textContent=this.locationName;
-        for(let j=0 ; j <hours.length ; j++)
-        {
-            let lie1=document.createElement('li');
-            ule1.appendChild(lie1);
-            if(TimeRanges.length -1 === j)
-             {
-                lie1.textContent=`${hours[j]} : ${this.total} cookies`;
-            }
-                lie1.textContent=`${hours[j]} : ${this.sales[j]} cookies`;
-                this.total=this.total+this.sales[j];
-                console.log(this.sales[j]);
-        }
-        let liRsults=document.createElement('li');
-        ule1.appendChild(liRsults)
-        liRsults.textContent=`total : ${this.total}`;
-    }
-};
+tokyo.Locations();
+let  dubai = new SalmonCookies('Dubai',11,38,3.7);
+dubai.Locations();
 
+let paris = new SalmonCookies('Paris',3,24,1.2);
+paris.Locations();
 
-Paris.getCoustomerPerHour();
-Paris.getCokiesAvg();
+let lima = new SalmonCookies('Lima',3,24,1.2);
+lima.Locations();
 
-Paris.render();
-// //--------------------------------------------------------------------------------
+console.log(locations );
+// for loop , array locations
 
-let Lima = {
-    locationName : 'Lima',
-    min_max_hourlyCoustomers:0,
-    Average_Cookies:4.6,
-    total:0,
-    sales:[],
-    customerPerHour:[],
-    getCust: function() {
-       return this.min_max_hourlyCoustomers= avgCust(2, 16) ;
-    },
-     getCookies: function(){
-       for(let i=0 ; i < hours.length ; i++)
-        {
-           this.sales.push(this.customerPerHour[i]*this.Average_Cookies);
-         }
-     },
-    getCoustomerPerHour: function()
-    {
-        for(let i = 0 ; i < hours.length ; i++){
-        this.customerPerHour.push(this.getCust());
-       console.log(this.customerPerHour[i]+" custome per hour")
-        }
-    },
-    getCokiesAvg : function()
-    {
-        for(let k = 0 ; k < hours.length ; k++){
-            this.getCoustomerPerHour();
-            let resulets=Math.floor(this.customerPerHour[k] * this.Average_Cookies )
-            this.sales.push(resulets);
-            this.total+=resulets;
-       console.log(this.Average_Cookies);
-        console.log(this.sales[k]+" cookies sold")
-        }
-    },
-    render : function()
-    {
-        let pe1=document.createElement('p');
-        list.appendChild(pe1);
-        let ule1=document.createElement('ul')
-        list.appendChild(ule1);
-        pe1.textContent=this.locationName;
-        for(let j=0 ; j <hours.length ; j++)
-        {
-            let lie1=document.createElement('li');
-            ule1.appendChild(lie1);
-            if(TimeRanges.length -1 === j)
-             {
-                lie1.textContent=`${hours[j]} : ${this.total} cookies`;
-            }
-                lie1.textContent=`${hours[j]} : ${this.sales[j]} cookies`;
-                this.total=this.total+this.sales[j];
-                console.log(this.sales[j]);
-        }
-        let liRsults=document.createElement('li');
-        ule1.appendChild(liRsults)
-        liRsults.textContent=`total : ${this.total}`;
-    }
-};
-
-Lima.getCoustomerPerHour();
-Lima.getCokiesAvg();
-
-Lima.render();
-
+for (let i = 0; i < locations.length; i++) {
+    console.log(locations[i].locationName);
+    console.log(locations[i].minCustPerHour);
+    console.log(locations[i].maxCustPerHour);
+    console.log(locations[i].avgCookiesPerCust);
+}
